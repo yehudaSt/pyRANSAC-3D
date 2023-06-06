3523c44298088c56a671546b333f71c57b8e8875
yehuda.stiefel <yehuda.stiefel@cs-robotics.com>
2023-06-06 10:14:18 +0300

    trying to use multi threading

--- pyransac3d/base_parallel.py
+++ pyransac3d/base_parallel.py
@@ -1,7 +1,17 @@
+from abc import ABC, abstractmethod
+from concurrent.futures.thread import ThreadPoolExecutor
+from random import Random
+from typing import List, Tuple
+
+import numpy as np
+
+
 class BaseParallelRansac(ABC):
     def __init__(self, seed=None, n_workers=None):
-        self.executor = ThreadPoolExecutor(max_workers=None)
+        self.executor = ThreadPoolExecutor(max_workers=n_workers)
         self.random = Random(seed)
+        self.inliers = []
+        self.equation = []
 
     def fit(
         self, points: np.ndarray, thresh: float = 0.05, max_iteration: int = 5000
@@ -14,11 +24,15 @@ class BaseParallelRansac(ABC):
             best_eq: A list of integers representing the best 'equation' for the primitive shape.
             best_inliers: A list of indices of points that fit the shape.
         """
-        best_eq = []
-        best_inliers = []
         jobs = ((self.random, points, float(thresh)) for _ in range(max_iteration))
         for eq, point_id_inliers in self.executor.map(self.iteration, *zip(*jobs)):
-            if len(point_id_inliers) > len(best_inliers):
-                best_eq = eq
-                best_inliers = point_id_inliers
-        return best_eq, best_inliers
+            if len(point_id_inliers) > len(self.best_inliers):
+                self.best_eq = eq
+                self.best_inliers = point_id_inliers
+        return self.best_eq, self.best_inliers
+
+    @abstractmethod
+    def iteration(
+        self, random: Random, pts: np.ndarray, thresh: float
+    ) -> Tuple[List[int], List[int]]:
+        pass
--- pyransac3d/cuboid.py
+++ pyransac3d/cuboid.py
@@ -2,8 +2,10 @@ import random
 
 import numpy as np
 
+from pyransac3d.base_parallel import BaseParallelRansac
 
-class Cuboid:
+
+class Cuboid(BaseParallelRansac):
     """
     Implementation for box (Cuboid) RANSAC.
 
@@ -17,105 +19,176 @@ class Cuboid:
     ---
     """
 
-    def __init__(self):
-        self.inliers = []
-        self.equation = []
-
-    def fit(self, pts, thresh=0.05, maxIteration=5000):
-        """
-        Find the best equation for 3 planes which define a complete cuboid.
-
-        :param pts: 3D point cloud as a `np.array (N,3)`.
-        :param thresh: Threshold distance from the cylinder radius which is considered inlier.
-        :param maxIteration: Number of maximum iteration which RANSAC will loop over.
-        :returns:
-        - `best_eq`:  Array of 3 best planes's equation `np.array (3, 4)`
-        - `best_inliers`: Inlier's index from the original point cloud. `np.array (1, M)`
-        ---
-        """
+    def iteration(self, random: random.Random, pts: np.ndarray, thresh: float):
         n_points = pts.shape[0]
-        best_eq = []
-        best_inliers = []
 
-        for it in range(maxIteration):
-            plane_eq = []
+        # Samples 6 random points
+        id_samples = random.sample(range(0, n_points), 6)
+        pt_samples = pts[id_samples]
 
-            # Samples 6 random points
-            id_samples = random.sample(range(0, n_points), 6)
-            pt_samples = pts[id_samples]
+        # We have to find the plane equation described by those 3 points
+        # We find first 2 vectors that are part of this plane
+        # A = pt2 - pt1
+        # B = pt3 - pt1
 
-            # We have to find the plane equation described by those 3 points
-            # We find first 2 vectors that are part of this plane
-            # A = pt2 - pt1
-            # B = pt3 - pt1
+        vecA = pt_samples[1, :] - pt_samples[0, :]
+        vecB = pt_samples[2, :] - pt_samples[0, :]
 
-            vecA = pt_samples[1, :] - pt_samples[0, :]
-            vecB = pt_samples[2, :] - pt_samples[0, :]
+        # Now we compute the cross product of vecA and vecB to get vecC which is normal to the plane
+        vecC = np.cross(vecA, vecB)
 
-            # Now we compute the cross product of vecA and vecB to get vecC which is normal to the plane
-            vecC = np.cross(vecA, vecB)
+        # The plane equation will be vecC[0]*x + vecC[1]*y + vecC[0]*z = -k
+        # We have to use a point to find k
+        vecC = vecC / np.linalg.norm(vecC)  # Normal
 
-            # The plane equation will be vecC[0]*x + vecC[1]*y + vecC[0]*z = -k
-            # We have to use a point to find k
-            vecC = vecC / np.linalg.norm(vecC)  # Normal
+        k = -np.sum(np.multiply(vecC, pt_samples[1, :]))
+        plane_eq = np.hstack(vecC, k)
 
-            k = -np.sum(np.multiply(vecC, pt_samples[1, :]))
-            plane_eq.append([vecC[0], vecC[1], vecC[2], k])
+        # Now we use another point to find a orthogonal plane 2
+        # Calculate distance from the point to the first plane
+        dist_p4_plane = (
+                                plane_eq[0][0] * pt_samples[3, 0]
+                                + plane_eq[0][1] * pt_samples[3, 1]
+                                + plane_eq[0][2] * pt_samples[3, 2]
+                                + plane_eq[0][3]
+                        ) / np.sqrt(plane_eq[0][0] ** 2 + plane_eq[0][1] ** 2 + plane_eq[0][2] ** 2)
 
-            # Now we use another point to find a orthogonal plane 2
-            # Calculate distance from the point to the first plane
-            dist_p4_plane = (
-                plane_eq[0][0] * pt_samples[3, 0]
-                + plane_eq[0][1] * pt_samples[3, 1]
-                + plane_eq[0][2] * pt_samples[3, 2]
-                + plane_eq[0][3]
-            ) / np.sqrt(plane_eq[0][0] ** 2 + plane_eq[0][1] ** 2 + plane_eq[0][2] ** 2)
+        # vecC is already normal (module 1) so we only have to discount from the point, the distance*unity = distance*normal
+        # A simple way of understanding this is we move our point along the normal until it reaches the plane
+        p4_proj_plane = pt_samples[3, :] - dist_p4_plane * vecC
 
-            # vecC is already normal (module 1) so we only have to discount from the point, the distance*unity = distance*normal
-            # A simple way of understanding this is we move our point along the normal until it reaches the plane
-            p4_proj_plane = pt_samples[3, :] - dist_p4_plane * vecC
+        # Now, with help of our point p5 we can find another plane P2 which contains p4, p4_proj, p5 and
+        vecD = p4_proj_plane - pt_samples[3, :]
+        vecE = pt_samples[4, :] - pt_samples[3, :]
+        vecF = np.cross(vecD, vecE)
+        vecF = vecF / np.linalg.norm(vecF)  # Normal
+        k = -np.sum(np.multiply(vecF, pt_samples[4, :]))
+        plane_eq_2 = np.hstack(vecF, k)
+        plane_eq = np.vstack(plane_eq, plane_eq_2)
 
-            # Now, with help of our point p5 we can find another plane P2 which contains p4, p4_proj, p5 and
-            vecD = p4_proj_plane - pt_samples[3, :]
-            vecE = pt_samples[4, :] - pt_samples[3, :]
-            vecF = np.cross(vecD, vecE)
-            vecF = vecF / np.linalg.norm(vecF)  # Normal
-            k = -np.sum(np.multiply(vecF, pt_samples[4, :]))
-            plane_eq.append([vecF[0], vecF[1], vecF[2], k])
+        # The last plane will be orthogonal to the first and sacond plane (and its normals will be orthogonal to first and second planes' normal)
+        vecG = np.cross(vecC, vecF)
 
-            # The last plane will be orthogonal to the first and sacond plane (and its normals will be orthogonal to first and second planes' normal)
-            vecG = np.cross(vecC, vecF)
+        k = -np.sum(np.multiply(vecG, pt_samples[5, :]))
+        plane_eq_2 = np.hstack(vecG, k)
+        plane_eq = np.vstack(plane_eq, plane_eq_2)
+        # We have to find the value D for the last plane.
 
-            k = -np.sum(np.multiply(vecG, pt_samples[5, :]))
-            plane_eq.append([vecG[0], vecG[1], vecG[2], k])
-            plane_eq = np.asarray(plane_eq)
-            # We have to find the value D for the last plane.
-
-            # Distance from a point to a plane
-            # https://mathworld.wolfram.com/Point-PlaneDistance.html
-            pt_id_inliers = []  # list of inliers ids
-            dist_pt = []
-            for id_plane in range(plane_eq.shape[0]):
-                dist_pt.append(
-                    np.abs(
-                        (
+        # Distance from a point to a plane
+        # https://mathworld.wolfram.com/Point-PlaneDistance.html
+        dist_pt = []
+        for id_plane in range(plane_eq.shape[0]):
+            dist_pt.append(
+                np.abs(
+                    (
                             plane_eq[id_plane, 0] * pts[:, 0]
                             + plane_eq[id_plane, 1] * pts[:, 1]
                             + plane_eq[id_plane, 2] * pts[:, 2]
                             + plane_eq[id_plane, 3]
-                        )
-                        / np.sqrt(plane_eq[id_plane, 0] ** 2 + plane_eq[id_plane, 1] ** 2 + plane_eq[id_plane, 2] ** 2)
                     )
+                    / np.sqrt(plane_eq[id_plane, 0] ** 2 + plane_eq[id_plane, 1] ** 2 + plane_eq[id_plane, 2] ** 2)
                 )
+            )
 
-            # Select indexes where distance is biggers than the threshold
-            dist_pt = np.asarray(dist_pt)
-            min_dist_pt = np.amin(dist_pt, axis=0)
-            pt_id_inliers = np.where(np.abs(min_dist_pt) <= thresh)[0]
+        # Select indexes where distance is biggers than the threshold
+        dist_pt = np.asarray(dist_pt)
+        min_dist_pt = np.amin(dist_pt, axis=0)
+        pt_id_inliers = np.where(np.abs(min_dist_pt) <= thresh)[0]
+        return plane_eq, pt_id_inliers
 
-            if len(pt_id_inliers) > len(best_inliers):
-                best_eq = plane_eq
-                best_inliers = pt_id_inliers
-            self.inliers = best_inliers
-            self.equation = best_eq
-        return best_eq, best_inliers
+    # def fit(self, pts, thresh=0.05, maxIteration=5000):
+    #     """
+    #     Find the best equation for 3 planes which define a complete cuboid.
+    #
+    #     :param pts: 3D point cloud as a `np.array (N,3)`.
+    #     :param thresh: Threshold distance from the cylinder radius which is considered inlier.
+    #     :param maxIteration: Number of maximum iteration which RANSAC will loop over.
+    #     :returns:
+    #     - `best_eq`:  Array of 3 best plane's equation `np.array (3, 4)`
+    #     - `best_inliers`: Inlier's index from the original point cloud. `np.array (1, M)`
+    #     ---
+    #     """
+    #     n_points = pts.shape[0]
+    #
+    #     for it in range(maxIteration):
+    #         plane_eq = []
+    #
+    #         # Samples 6 random points
+    #         id_samples = random.sample(range(0, n_points), 6)
+    #         pt_samples = pts[id_samples]
+    #
+    #         # We have to find the plane equation described by those 3 points
+    #         # We find first 2 vectors that are part of this plane
+    #         # A = pt2 - pt1
+    #         # B = pt3 - pt1
+    #
+    #         vecA = pt_samples[1, :] - pt_samples[0, :]
+    #         vecB = pt_samples[2, :] - pt_samples[0, :]
+    #
+    #         # Now we compute the cross product of vecA and vecB to get vecC which is normal to the plane
+    #         vecC = np.cross(vecA, vecB)
+    #
+    #         # The plane equation will be vecC[0]*x + vecC[1]*y + vecC[0]*z = -k
+    #         # We have to use a point to find k
+    #         vecC = vecC / np.linalg.norm(vecC)  # Normal
+    #
+    #         k = -np.sum(np.multiply(vecC, pt_samples[1, :]))
+    #         plane_eq.append([vecC[0], vecC[1], vecC[2], k])
+    #
+    #         # Now we use another point to find a orthogonal plane 2
+    #         # Calculate distance from the point to the first plane
+    #         dist_p4_plane = (
+    #             plane_eq[0][0] * pt_samples[3, 0]
+    #             + plane_eq[0][1] * pt_samples[3, 1]
+    #             + plane_eq[0][2] * pt_samples[3, 2]
+    #             + plane_eq[0][3]
+    #         ) / np.sqrt(plane_eq[0][0] ** 2 + plane_eq[0][1] ** 2 + plane_eq[0][2] ** 2)
+    #
+    #         # vecC is already normal (module 1) so we only have to discount from the point, the distance*unity = distance*normal
+    #         # A simple way of understanding this is we move our point along the normal until it reaches the plane
+    #         p4_proj_plane = pt_samples[3, :] - dist_p4_plane * vecC
+    #
+    #         # Now, with help of our point p5 we can find another plane P2 which contains p4, p4_proj, p5 and
+    #         vecD = p4_proj_plane - pt_samples[3, :]
+    #         vecE = pt_samples[4, :] - pt_samples[3, :]
+    #         vecF = np.cross(vecD, vecE)
+    #         vecF = vecF / np.linalg.norm(vecF)  # Normal
+    #         k = -np.sum(np.multiply(vecF, pt_samples[4, :]))
+    #         plane_eq.append([vecF[0], vecF[1], vecF[2], k])
+    #
+    #         # The last plane will be orthogonal to the first and sacond plane (and its normals will be orthogonal to first and second planes' normal)
+    #         vecG = np.cross(vecC, vecF)
+    #
+    #         k = -np.sum(np.multiply(vecG, pt_samples[5, :]))
+    #         plane_eq.append([vecG[0], vecG[1], vecG[2], k])
+    #         plane_eq = np.asarray(plane_eq)
+    #         # We have to find the value D for the last plane.
+    #
+    #         # Distance from a point to a plane
+    #         # https://mathworld.wolfram.com/Point-PlaneDistance.html
+    #         pt_id_inliers = []  # list of inliers ids
+    #         dist_pt = []
+    #         for id_plane in range(plane_eq.shape[0]):
+    #             dist_pt.append(
+    #                 np.abs(
+    #                     (
+    #                         plane_eq[id_plane, 0] * pts[:, 0]
+    #                         + plane_eq[id_plane, 1] * pts[:, 1]
+    #                         + plane_eq[id_plane, 2] * pts[:, 2]
+    #                         + plane_eq[id_plane, 3]
+    #                     )
+    #                     / np.sqrt(plane_eq[id_plane, 0] ** 2 + plane_eq[id_plane, 1] ** 2 + plane_eq[id_plane, 2] ** 2)
+    #                 )
+    #             )
+    #
+    #         # Select indexes where distance is biggers than the threshold
+    #         dist_pt = np.asarray(dist_pt)
+    #         min_dist_pt = np.amin(dist_pt, axis=0)
+    #         pt_id_inliers = np.where(np.abs(min_dist_pt) <= thresh)[0]
+    #
+    #         if len(pt_id_inliers) > len(best_inliers):
+    #             best_eq = plane_eq
+    #             best_inliers = pt_id_inliers
+    #         self.inliers = best_inliers
+    #         self.equation = best_eq
+    #     return best_eq, best_inliers
